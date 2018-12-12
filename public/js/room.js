21bf79a9117704846b0f7bf9457a3415b2e66da0
Hieu Nguyen <hieunv.ce@gmail.com>
2018-12-12 23:36:26 +0700

    Finish room

--- public/js/room.js
+++ public/js/room.js
@@ -19,19 +19,52 @@ $(document).ready(function () {
     console.log("Cookie username: " + logedInUser);
 
     var socket = io("http://localhost:3000");
-    socket.emit("username", logedInUser);
+    socket.emit("room-username", logedInUser);
 
     socket.on("roomList", function (data) {
         $("#roomList").html("");
         data.forEach(room => {
-            $("#roomList").append(
-                '<div class="room">' + room + '</div>'
-            );
+            if (room.n < 2) {
+                $("#roomList").append('<div class="room">'+ room.name + '</div>');
+            }
         });
     });
 
     socket.on("playerInfo", function (playerInfo) {
-        console.log("Player info: " + playerInfo);
-        $("#playername").html(playerInfo.username);
+        console.log("Player info: " + JSON.stringify(playerInfo));
+        if (playerInfo.username === "!!null!!") {
+            $("#playername").html("Null player. Please relogin!");
+        } else {
+            $("#playername").html(playerInfo.username);
+        }
+
+    });
+
+    socket.on("logedOut", function () {
+        window.location = '/';
+    });
+
+    socket.on("goToNewRoomBro", function (newRoom) {
+        document.cookie = "userRoom=" + newRoom + ";path=/";
+        window.location = '/setship';
+    });
+
+    $("#logout").click(function () {
+        socket.emit("room-logout");
+    });
+
+    $("#btnCreateNewRoom").click(function () {
+        let newRoom = $("#newRoomName").val();
+        if (newRoom != "") {
+            socket.emit("room-gotoRoom", newRoom);
+        } else {
+            $("#newRoomName").attr("placeholder", "Please enter room name");
+        }
+    });
+
+    $(document).on("click", "#roomList div.room", function () {
+        let roomName = $(this).text();
+        console.log("Room Name " + roomName + " has been clicked");
+        socket.emit("room-gotoRoom", roomName);
     });
 });
--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(urlencodedParser);
 
 // Mang danh sach cac nguoi choi online
 var onlinePlayers = [];
-var roomList = ["lobby"];
+var roomList = [];
 
 server.listen(3000);
 console.log("Server is listening at port: 3000");
@@ -22,13 +22,68 @@ io.on("connection", function (socket) {
     console.log("ID connected: " + socket.id);
     socket.join("lobby");
 
-    socket.on("username",function(data){
+    socket.on("room-username",function(data){
         console.log("Loged in player: "+data);
         let playerInfo = onlinePlayers.find(player => player.username == data);
-        socket.emit("playerInfo",playerInfo);
-        console.log("Online players: "+JSON.stringify(onlinePlayers));
+        if (playerInfo != null && playerInfo != undefined){
+            onlinePlayers[onlinePlayers.indexOf(playerInfo)]["room"]="lobby";
+            socket.username = data;
+            socket.emit("playerInfo",playerInfo);
+            console.log("Online players: "+JSON.stringify(onlinePlayers));
+        } else {
+            let nullplayer={};
+            nullplayer.username="!!null!!"
+            socket.emit("playerInfo",nullplayer);
+        }
     });
     socket.emit("roomList",roomList);
+
+    socket.on("room-logout",function(){
+        // Remove player from online player list
+        let player = onlinePlayers.find(el => el.username == socket.username);
+        if (player != null && player != undefined){
+            onlinePlayers.splice(onlinePlayers.indexOf(player),1);
+        }
+        console.log("Loged out: "+socket.username);
+        console.log("Online players: "+JSON.stringify(onlinePlayers));
+        socket.emit("logedOut");
+    });
+
+    socket.on("room-gotoRoom",function(roomName){
+        
+        let room = roomList.find(el => el.name == roomName);
+        let status = false;
+
+        if (room==undefined || room==null){
+            room = {};
+            room.name = roomName;
+            room.p1 = socket.username;
+            room.p2 = "";
+            room.n=1;
+            socket.join(room.name);
+            status=true;
+        } else {
+            roomList.splice(roomList.indexOf(room),1);
+            if (room.p1==""){
+                room.p1=socket.username;
+                room.n=2;
+                status=true;
+            } else if (room.p2==""){
+                room.p2=socket.username;
+                room.n=2;
+                status=true;
+            } else {
+                socket.emit("joinRoomFail");
+                status=false;
+            }
+        }
+        if (status == true){
+        roomList.push(room);
+        socket.emit("goToNewRoomBro",room.name);
+        }
+        console.log("Room list: "+JSON.stringify(roomList));
+        socket.broadcast.emit("roomList",roomList);
+    });
 
     socket.on("disconnect",function(){
         console.log(socket.id + " has disconnected");
@@ -51,13 +106,17 @@ app.get("/room", function (req, res) {
     res.render("room");
 });
 
+app.get("/setship",function(req, res){
+    res.render("setship");
+});
+
 
 app.post('/login', urlencodedParser, function (req, res) {
 
     let username = req.body.username;
     let password = req.body.password;
     let newPlayer={};
-    newPlayer["username"] = "hieu";
+    newPlayer["username"] = req.body.username;
     console.log("New player: "+JSON.stringify(newPlayer));
     db.selectPlayer(username, password, function (isExist) {
         // If account exists in database
